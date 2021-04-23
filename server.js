f896c8f4cd87d8965f218118f235cb25af917464
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-04-23 19:02:01 -0400

    deleted htmlroute.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 require('./apiroute')(app);
+require('./htmlroute')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
